b00c5166e31d9de84e3e350d38ccb7017f336f2b
Laura Vitória <lvsa@cesar.school>
2026-08-31 13:57:21 -0300

    separa funções calcular_pixel e salvar_pgm

--- main.c
+++ main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int calcular_pixel(double cr, double ci, int interacao){
+    double zr = 0;
+    double zi = 0;
+    int n;
+    for (n = 0; n < interacao; n++){
+        double zr_novo = zr * zr - zi * zi + cr;
+        double zi_novo = 2 * zr * zi + ci;
+        zr = zr_novo;
+        zi = zi_novo;
+        if (zr * zr + zi * zi > 4.0){
+            break;
+        }
+    }
+    return n;
+}
+
+void salvar_pgm(int **matriz, int altura, int largura, int interacao){
+    FILE *arquivo = fopen("mandelbrot.pgm", "w");
+    
+    if (arquivo == NULL){
+        printf("Erro ao abrir arquivo mandelbrot.pgm\n");
+        return;
+    }
+    
+    fprintf(arquivo, "P2\n%d %d\n255\n", largura, altura);
+    
+    for (int i = 0; i < altura; i++){
+        for (int j = 0; j < largura; j++){
+            int pixel = (matriz[i][j] * 255) / interacao;
+            fprintf(arquivo, "%d ", pixel);
+        }
+        fprintf(arquivo, "\n");
+    }
+    fclose(arquivo);
+}
+
 int main(int argc, char *argv[]){
 
     if (argc < 5 || argc > 5){
@@ -15,6 +51,7 @@ int main(int argc, char *argv[]){
     
         if (altura < 1 || altura > 800 || largura < 1 || largura > 600 || interacao < 1 || interacao > 100 || threads < 1 || threads > 4){
             printf("você excedeu algum limite de valor");
+            return 1;
         }
         else{
             int **matriz = (int**)malloc(altura * sizeof(int*));
@@ -29,34 +66,11 @@ int main(int argc, char *argv[]){
                     double cr = -2.0 + j * (3.0 / largura);
                     double ci = -1.5 + i * (3.0 / altura);
 
-                    double zr = 0;
-                    double zi = 0;
-                    int n;
-                    for (n = 0; n < interacao; n++){
-                        double zr_novo = zr * zr - zi * zi + cr;
-                        double zi_novo = 2 * zr * zi + ci;
-                        zr = zr_novo;
-                        zi = zi_novo;
-                        if (zr * zr + zi * zi > 4.0){
-                            break;
-                        }
-                    }
-                    matriz[i][j] = n;
+                    matriz[i][j] = calcular_pixel(cr, ci, interacao);
                 }
             }
             
-            FILE *arquivo = fopen("mandelbrot.pgm", "w");
-            fprintf(arquivo, "P2\n%d %d\n255\n", largura, altura);
-            
-            for (i = 0; i < altura; i++){
-                for (j = 0; j < largura; j++){
-                    int pixel = (matriz[i][j] * 255) / interacao;
-                    fprintf(arquivo, "%d ", pixel);
-                }
-                fprintf(arquivo, "\n");
-            }
-            fclose(arquivo);
-            
+            salvar_pgm(matriz, altura, largura, interacao);
             printf("Imagem salva em mandelbrot.pgm\n");
             
             for (int k = 0; k < altura; k++){
