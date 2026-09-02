c9430ff4549929f86049cef6352ab0cf63a30e86
Laura Vitória <lvsa@cesar.school>
2026-09-01 21:21:02 -0300

    implementa execução com pthreads

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <omp.h>
+#include <pthread.h>
+
+typedef struct DadosThread{
+    int **matriz;
+    int altura;
+    int largura;
+    int interacao;
+    int linha_inicio;
+    int linha_fim;
+} DadosThread;
 
 int calcular_pixel(double cr, double ci, int interacao){
     double zr = 0;
@@ -106,6 +116,76 @@ int** executar_mandelbrot_openmp(int altura, int largura, int interacao, int thr
     return matriz;
 }
 
+void *calcular_pthread(void *arg){
+    DadosThread *args = (DadosThread *)arg;
+    
+    for (int i = args->linha_inicio; i < args->linha_fim; i++){
+        for(int j = 0; j < args->largura; j++){
+            double cr = -2.0 + j * (3.0 / args->largura);
+            double ci = -1.5 + i * (3.0 / args->altura);
+
+            args->matriz[i][j] = calcular_pixel(cr, ci, args->interacao);
+        }
+    }
+    free(args);
+    return NULL;
+}
+
+int** executar_mandelbrot_pthread(int altura, int largura, int interacao, int threads){
+    int **matriz = (int**)malloc(altura * sizeof(int*));
+    
+    if (matriz == NULL){
+        printf("Erro ao alocar memória para matriz\n");
+        return NULL;
+    }
+    
+    for (int k = 0; k < altura; k++){
+        matriz[k] = (int*)malloc(largura * sizeof(int));
+        
+        if (matriz[k] == NULL){
+            printf("Erro ao alocar memória para linha %d\n", k);
+            for (int i = 0; i < k; i++){
+                free(matriz[i]);
+            }
+            free(matriz);
+            return NULL;
+        }
+    }
+    
+    pthread_t tids[threads];
+    DadosThread *dados[threads];
+    
+    int linhas_por_thread = altura / threads;
+    
+    for (int t = 0; t < threads; t++){
+        dados[t] = (DadosThread *)malloc(sizeof(DadosThread));
+        
+        dados[t]->matriz = matriz;
+        dados[t]->altura = altura;
+        dados[t]->largura = largura;
+        dados[t]->interacao = interacao;
+        dados[t]->linha_inicio = t * linhas_por_thread;
+        dados[t]->linha_fim = (t == threads - 1) ? altura : (t + 1) * linhas_por_thread;
+        
+        int ret = pthread_create(&tids[t], NULL, calcular_pthread, (void *)dados[t]);
+        
+        if (ret != 0){
+            printf("Erro ao criar thread %d\n", t);
+            return NULL;
+        }
+    }
+    
+    for (int t = 0; t < threads; t++){
+        int ret = pthread_join(tids[t], NULL);
+        
+        if (ret != 0){
+            printf("Erro ao aguardar thread %d\n", t);
+        }
+    }
+    
+    return matriz;
+}
+
 int main(int argc, char *argv[]){
 
     if (argc < 5 || argc > 5){
@@ -128,7 +208,7 @@ int main(int argc, char *argv[]){
             struct timespec fim;
             clock_gettime(CLOCK_MONOTONIC, &inicio);
 
-            int **matriz = executar_mandelbrot_openmp(altura, largura, interacao, threads);
+            int **matriz = executar_mandelbrot_pthread(altura, largura, interacao, threads);
             if (matriz == NULL){
                 return 1;
             }
@@ -138,7 +218,7 @@ int main(int argc, char *argv[]){
             double tempo = (fim.tv_sec - inicio.tv_sec) +
                (fim.tv_nsec - inicio.tv_nsec) / 1000000000.0;
 
-            printf("Tempo OpenMP: %f segundos\n", tempo);
+            printf("Tempo Pthread: %f segundos\n", tempo);
             
             salvar_pgm(matriz, altura, largura, interacao);
             printf("Imagem salva em mandelbrot.pgm\n");
